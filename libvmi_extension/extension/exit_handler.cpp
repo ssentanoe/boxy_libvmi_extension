c01f929ed9e4a424558029f683343cc795885c90
Stewart Sentanoe <stewart.sentanoe@gmail.com>
2019-06-07 21:00:52 +0200

    test 2

--- libvmi_extension/extension/exit_handler.cpp
+++ libvmi_extension/extension/exit_handler.cpp
@@ -1,4 +1,7 @@
+#include <bfvmm/vcpu/vcpu_factory.h>
+#include <hve/arch/intel_x64/vcpu.h>
 #include <boxy/hve/arch/intel_x64/vcpu.h>
+#include <boxy/domain/domain_factory.h>
 #include <boxy/domain/domain.h>
 #include <bfdebug.h>
 
@@ -12,3 +15,40 @@ public:
 		bfdebug_info(0, "extension loaded");		
 	}
 };
+
+namespace bfvmm
+{
+
+std::unique_ptr<vcpu>
+vcpu_factory::make(vcpuid::type vcpuid, bfobject *obj)
+{
+	static domain dom0{0};
+	if (vcpuid::is_host_vm_vcpu(vcpuid))
+	{
+		return
+		std::make_unique<vmi_vcpu>(
+			vcpuid, dynamic_cast<domain *>(&dom0)
+		);
+	}
+	else
+	{
+		return
+		std::make_unique<vmi_vcpu>(
+			vcpuid, dynamic_cast<domain *>(obj)
+		);
+	}
+}
+
+}
+
+namespace boxy
+{
+
+std::unique_ptr<domain>
+domain_factory::make(domain::domainid_type domainid, bfobject *obj)
+{
+    bfignored(obj);
+    return std::make_unique<boxy::intel_x64::domain>(domainid);
+}
+
+}
