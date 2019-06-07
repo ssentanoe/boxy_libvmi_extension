2eaa7864aa3c32bd8236a4c53b292b8304c17a2d
Stewart Sentanoe <stewart.sentanoe@gmail.com>
2019-06-07 20:34:15 +0200

    test

--- libvmi_extension/extension/exit_handler.cpp
+++ libvmi_extension/extension/exit_handler.cpp
@@ -1,6 +1,14 @@
 #include <boxy/hve/arch/intel_x64/vcpu.h>
+#include <boxy/domain/domain.h>
+#include <bfdebug.h>
+
+using namespace boxy::intel_x64;
 
 class vmi_vcpu : public boxy::intel_x64::vcpu
 {
-
+public:
+	vmi_vcpu(vcpuid::type id, gsl::not_null<domain *> domain) : boxy::intel_x64::vcpu{id, domain}
+	{
+		bfdebug_info(0, "extension loaded");		
+	}
 };
